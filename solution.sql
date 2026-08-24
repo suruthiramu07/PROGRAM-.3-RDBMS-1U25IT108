54e67eaae14b9cb89f85d0d3d3a8fad2232efbc8
SURUTHI R <ssuruthiramu@gmail.com>
2026-08-24 10:03:52 +0530

    PROGRAM .3 RDBMS 1U25IT108

--- solution.sql
+++ solution.sql
@@ -1,1 +1,18 @@
 
+CREATE DATABASE collegeDB;
+USE CollegeDB;
+CREATE TABLE Department (
+    StudentID INT(5) NOT NULL PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+);
+desc Department;
+
+ALTER TABLE Student
+ADD(
+  Email VARCHAR(30),
+  PhoneNumber NUMERIC(10)
+);
+desc Student;
